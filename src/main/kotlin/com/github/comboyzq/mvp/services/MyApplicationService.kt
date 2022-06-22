1952a75b1d3f4f5856ff1c0fd4cac876d682bdc9
GitHub Action <action@github.com>
2022-06-22 05:39:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/comboyzq/mvp/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/comboyzq/mvp/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.comboyzq.mvp.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.comboyzq.mvp.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/comboyzq/mvp/services/MyProjectService.kt
+++ src/main/kotlin/com/github/comboyzq/mvp/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.comboyzq.mvp.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.comboyzq.mvp.MyBundle
 
 class MyProjectService(project: Project) {
 
